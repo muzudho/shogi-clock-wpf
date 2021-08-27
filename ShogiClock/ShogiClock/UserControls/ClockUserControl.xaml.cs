f0caad9b689748bd694b5eed588f352eafbab437
むずでょ <muzudho1@gmail.com>
2021-08-27 20:32:10 +0900

    ワーカースレッド

--- ShogiClock/ShogiClock/UserControls/ClockUserControl.xaml.cs
+++ ShogiClock/ShogiClock/UserControls/ClockUserControl.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using ShogiClock.ViewModels;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 
 namespace ShogiClock.UserControls
@@ -13,11 +15,60 @@ namespace ShogiClock.UserControls
             InitializeComponent();
         }
 
+        /// <summary>
+        /// 監視ボタン クリック時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            // このコードブロックはUIスレッド。UIにアクセスできますが、処理は早く終わらなければいけません
             var viewModel = this.DataContext as ClockViewModel;
+
+            // URLを取得
+            var url = this.urlTextBox.Text;
+
+            // ビューに表示
             viewModel.FirstPlayerText = "先手WIP";
             viewModel.SecondPlayerText = "後手WIP";
+
+            // ワーカースレッドを起動します
+            var start = new ThreadStart(MonitoringThread);
+            var thread = new Thread(start);
+            thread.Start();
         }
+
+        /// <summary>
+        /// いわゆるワーカースレッド。
+        /// URLを監視するループです。
+        /// </summary>
+        private void MonitoringThread()
+        {
+            // このコードブロックは ワーカー スレッド。UIにはアクセスできません
+
+            // TODO CSAファイル読取
+
+            // TODO ループ
+            // 時間のかかる処理
+            for (; ; )
+            {
+                // 時間のかかる処理
+                Thread.Sleep(3 * 1000);
+
+                // すぐ終わる処理
+                Task.Run(() => this.firstPlayerLabel.Dispatcher.Invoke(() =>
+                {
+                    // このコードブロックは UIスレッド。UIを更新できます
+                    var viewModel = this.DataContext as ClockViewModel;
+                    viewModel.FirstPlayerText += "a";
+                }));
+            }
+
+            //this.Dispatcher.Invoke((Action)(() =>
+            //{
+            //    this.FirstPlayerText = "Good bye!";
+            //}));
+        }
+
     }
 }
--- ShogiClock/ShogiClock/ViewModels/ClockViewModel.cs
+++ ShogiClock/ShogiClock/ViewModels/ClockViewModel.cs
@@ -1,9 +1,34 @@
 ﻿using Livet;
+using System;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace ShogiClock.ViewModels
 {
     public class ClockViewModel : ViewModel
     {
+        private string _urlText = string.Empty;
+
+        /// <summary>
+        /// URLのテキスト
+        /// </summary>
+        public string UrlText
+        {
+            get
+            {
+                return this._urlText;
+            }
+            set
+            {
+                if (this._urlText == value)
+                {
+                    return;
+                }
+                this._urlText = value;
+                RaisePropertyChanged("UrlText");
+            }
+        }
+
         private string _firstPlayerText = string.Empty;
 
         /// <summary>
