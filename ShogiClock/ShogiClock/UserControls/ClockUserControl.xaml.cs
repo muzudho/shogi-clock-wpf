7121bd8e9b6a9cfd56b2e0059d76b881ceed5130
むずでょ <muzudho1@gmail.com>
2021-08-28 00:38:18 +0900

    タスクの中断でそのまま終わるように

--- ShogiClock/ShogiClock/UserControls/ClockUserControl.xaml.cs
+++ ShogiClock/ShogiClock/UserControls/ClockUserControl.xaml.cs
@@ -25,76 +25,83 @@ namespace ShogiClock.UserControls
         private void MonitoringThread()
         {
             // このコードブロックは ワーカー スレッド。UIにはアクセスできません
-            //try
-            //{
-            // TODO ループ
-            for (; ; )
+            try
             {
-                // UIから URL取得
-                // Example: https://golan.sakura.ne.jp/denryusen/dr2_tsec/kifufiles/dr2tsec+buoy_james8nakahi_dr2b3-11-bottom_43_dlshogi_xylty-60-2F+dlshogi+xylty+20210718131042.csa
-                (string url, string tournament, int intervalSeconds) = Task.Run(() => this.firstPlayerLabel.Dispatcher.Invoke(() =>
+                for (; ; )
                 {
+                    // UIから URL取得
+                    // Example: https://golan.sakura.ne.jp/denryusen/dr2_tsec/kifufiles/dr2tsec+buoy_james8nakahi_dr2b3-11-bottom_43_dlshogi_xylty-60-2F+dlshogi+xylty+20210718131042.csa
+                    (string url, string tournament, int intervalSeconds) = Task.Run(() => this.firstPlayerLabel.Dispatcher.Invoke(() =>
+                    {
                     // このコードブロックは UIスレッド。UIを更新できます
                     var viewModel = this.DataContext as ClockViewModel;
-                    return (viewModel.UrlText, viewModel.Tournament.CurrentItem as string, viewModel.IntervalSeconds);
-                })).Result;
+                        var sec = viewModel.IntervalSeconds;
+                        if (sec < 1)
+                        {
+                        // とりあえず、最低でも1秒に強制しとこ
+                        sec = 1;
+                            viewModel.IntervalSeconds = sec;
+                        }
+                        return (viewModel.UrlText, viewModel.Tournament.CurrentItem as string, sec);
+                    })).Result;
 
-                // CSAファイル読取
-                CsaFile csaFile;
-                if (CsaFile.Load(tournament, url, out csaFile))
-                {
-                    // 読込成功時
-                    // すぐ終わる処理
-                    bool finished = Task.Run(() => this.firstPlayerLabel.Dispatcher.Invoke(() =>
+                    // CSAファイル読取
+                    CsaFile csaFile;
+                    if (CsaFile.Load(tournament, url, out csaFile))
                     {
+                        // 読込成功時
+                        // すぐ終わる処理
+                        bool finished = Task.Run(() => this.firstPlayerLabel.Dispatcher.Invoke(() =>
+                        {
                         // このコードブロックは UIスレッド。UIを更新できます
                         var viewModel = this.DataContext as ClockViewModel;
-                        viewModel.FirstPlayerText = $"{csaFile.RemainingTime[1] / 60}分{csaFile.RemainingTime[1] % 60}秒";
-                        viewModel.SecondPlayerText = $"{csaFile.RemainingTime[2] / 60}分{csaFile.RemainingTime[2] % 60}秒";
+                            viewModel.FirstPlayerText = $"{csaFile.RemainingTime[1] / 60}分{csaFile.RemainingTime[1] % 60}秒";
+                            viewModel.SecondPlayerText = $"{csaFile.RemainingTime[2] / 60}分{csaFile.RemainingTime[2] % 60}秒";
 
-                        if (0 < csaFile.EndTime.Ticks)
+                            if (0 < csaFile.EndTime.Ticks)
+                            {
+                                viewModel.StatusText = $"{csaFile.EndTime.ToString("yyyy/MM/dd HH:mm:ss")}に対局終了。自動更新おわり  (最終更新 {DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")})";
+                                this.tournamentComboBox.IsEnabled = true;
+                                this.monitorButton.IsEnabled = true;
+                                this.intervalSeconds.IsEnabled = true;
+                                return true;
+                            }
+                            viewModel.StatusText = DateTime.Now.ToString("自動更新中 (最終更新 yyyy/MM/dd HH:mm:ss)");
+                            return false;
+                        })).Result;
+                        if (finished)
                         {
-                            viewModel.StatusText = $"{csaFile.EndTime.ToString("yyyy/MM/dd HH:mm:ss")}に対局終了。自動更新おわり  (最終更新 {DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")})";
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        // 読込成功時
+                        // すぐ終わる処理
+                        Task.Run(() => this.firstPlayerLabel.Dispatcher.Invoke(() =>
+                        {
+                        // このコードブロックは UIスレッド。UIを更新できます
+                        var viewModel = this.DataContext as ClockViewModel;
+                            viewModel.FirstPlayerText = $"先手----";
+                            viewModel.SecondPlayerText = $"後手----";
+                            viewModel.StatusText = DateTime.Now.ToString("棋譜を読み込めていません。自動更新おわり (最終更新 yyyy/MM/dd HH:mm:ss)");
                             this.tournamentComboBox.IsEnabled = true;
                             this.monitorButton.IsEnabled = true;
                             this.intervalSeconds.IsEnabled = true;
-                            return true;
-                        }
-                        viewModel.StatusText = DateTime.Now.ToString("自動更新中 (最終更新 yyyy/MM/dd HH:mm:ss)");
-                        return false;
-                    })).Result;
-                    if (finished)
-                    {
+                        })).Wait();
                         break;
                     }
-                }
-                else
-                {
-                    // 読込成功時
-                    // すぐ終わる処理
-                    Task.Run(() => this.firstPlayerLabel.Dispatcher.Invoke(() =>
-                    {
-                        // このコードブロックは UIスレッド。UIを更新できます
-                        var viewModel = this.DataContext as ClockViewModel;
-                        viewModel.FirstPlayerText = $"先手----";
-                        viewModel.SecondPlayerText = $"後手----";
-                        viewModel.StatusText = DateTime.Now.ToString("棋譜を読み込めていません。自動更新おわり (最終更新 yyyy/MM/dd HH:mm:ss)");
-                        this.tournamentComboBox.IsEnabled = true;
-                        this.monitorButton.IsEnabled = true;
-                        this.intervalSeconds.IsEnabled = true;
-                    })).Wait();
-                    break;
-                }
 
-
-                // 更新間隔（秒）
-                Thread.Sleep(intervalSeconds * 1000);
+                    // 更新間隔（秒）
+                    Thread.Sleep(intervalSeconds * 1000);
+                }
             }
-            //}
-            //catch (TaskCanceledException)
-            //{
-            //    // 単に終了します
-            //}
+            catch(System.AggregateException)
+            {
+                // TaskCanceledException
+                // ウィンドウを閉じたときなどに、タスクが中断してここを通ります。
+                // 単に終了します
+            }
 
             // Console.WriteLine("ワーカースレッド終了");
         }
