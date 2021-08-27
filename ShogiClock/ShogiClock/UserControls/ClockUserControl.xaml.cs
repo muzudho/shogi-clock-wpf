0fc06051a4e859289be309c8f457e011020d0d5e
むずでょ <muzudho1@gmail.com>
2021-08-27 19:25:04 +0900

    View

---
+++ ShogiClock/ShogiClock/UserControls/ClockUserControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ShogiClock.ViewModels;
+using System.Windows.Controls;
+
+namespace ShogiClock.UserControls
+{
+    /// <summary>
+    /// ClockUserControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ClockUserControl : UserControl
+    {
+        public ClockUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var viewModel = this.DataContext as ClockViewModel;
+            viewModel.FirstPlayerText = "先手WIP";
+            viewModel.SecondPlayerText = "後手WIP";
+        }
+    }
+}
---
+++ ShogiClock/ShogiClock/ViewModels/ClockViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Livet;
+
+namespace ShogiClock.ViewModels
+{
+    public class ClockViewModel : ViewModel
+    {
+        private string _firstPlayerText = string.Empty;
+
+        /// <summary>
+        /// 先手のテキスト
+        /// </summary>
+        public string FirstPlayerText
+        {
+            get
+            {
+                return this._firstPlayerText;
+            }
+            set
+            {
+                if (this._firstPlayerText == value)
+                {
+                    return;
+                }
+                this._firstPlayerText = value;
+                RaisePropertyChanged("FirstPlayerText");
+            }
+        }
+
+        private string _secondPlayerText = string.Empty;
+
+        /// <summary>
+        /// 後手のテキスト
+        /// </summary>
+        public string SecondPlayerText
+        {
+            get
+            {
+                return this._secondPlayerText;
+            }
+            set
+            {
+                if (this._secondPlayerText == value)
+                {
+                    return;
+                }
+                this._secondPlayerText = value;
+                RaisePropertyChanged("SecondPlayerText");
+            }
+        }
+    }
+}
